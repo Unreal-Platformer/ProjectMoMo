c38b86cb781445e06d7b08c091963647c4bf2ef2
Usofl <90086886+Usofl@users.noreply.github.com>
2023-11-07 23:49:21 +0900

    create mainmenu asset + class

---
+++ Source/Project_Momo/Private/Widget/MainMenu.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Widget/MainMenu.h"
+#include "Components/Button.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+void UMainMenu::StartNewGame()
+{
+	UGameplayStatics::OpenLevel(this, FName("Main"));
+}
+
+void UMainMenu::QuitGame()
+{
+	UKismetSystemLibrary::QuitGame(this, 0, EQuitPreference::Quit, false);
+}
+
+void UMainMenu::NativeConstruct()
+{
+	Super::NativeConstruct();
+	
+	if(NewGameButton)
+		NewGameButton->OnClicked.AddDynamic(this, &UMainMenu::StartNewGame);
+
+	if(QuitButton)
+		QuitButton->OnClicked.AddDynamic(this, &UMainMenu::QuitGame);
+}
